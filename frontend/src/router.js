5ea66c8aae4d905da86908519deddd2935e4349b
Han1015 <PRODXH1015@GMAIL.COM>
2019-12-09 20:07:21 +1100

    UI Component

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -10,7 +10,10 @@ import Loadings from './pages/ui/loadings';
 import Notice from './pages/ui/notice';
 import Messages from './pages/ui/messages';
 import NewTabs from './pages/ui/tabs';
-
+import Gallery from './pages/ui/gallery';
+import NewCarousel from './pages/ui/carousel';
+import FormLogin from './pages/form/formlogin';
+import FormRegister from './pages/form/formregister';
 export default class IRouter extends React.Component{
     render(){
         return(
@@ -27,6 +30,11 @@ export default class IRouter extends React.Component{
                                     <Route path="/admin/ui/notification" component={Notice}/>
                                     <Route path="/admin/ui/messages" component={Messages}/>
                                     <Route path="/admin/ui/tabs" component={NewTabs}/>
+                                    <Route path="/admin/ui/gallery" component={Gallery}/>
+                                    <Route path="/admin/ui/carousel" component={NewCarousel}/>
+                                    <Route path="/admin/form/login" component={FormLogin}/>
+                                    <Route path="/admin/form/reg" component={FormRegister}/>
+                                    
                                     <Route component={NoMatch}/>
                                 </Switch>
                             </Admin>
